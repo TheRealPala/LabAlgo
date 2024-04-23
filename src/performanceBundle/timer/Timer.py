d48fef25f1bc7767ef61adf1b6d279ee4171d42e
Alessandro Palano <apalano23@gmail.com>
2024-04-23 11:07:53 +0200

    FEAT: Added performanceBundle

---
+++ src/performanceBundle/timer/Timer.py
@@ -0,0 +1,20 @@
+from time import perf_counter
+
+
+def roundSeconds(seconds, decimals=3):
+    return round(seconds, decimals)
+
+
+def testFunction(function, args=None, decimals=3):
+    start = perf_counter()
+    if args is None:
+        function()
+    else:
+        function(args)
+    end = perf_counter()
+    return roundSeconds(end - start, decimals)
+
+
+class timer:
+    def __init__(self):
+        pass
