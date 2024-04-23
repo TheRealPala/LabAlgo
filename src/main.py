4ce040d618d9203271aeb91d02b35c26bd228223
Alessandro Palano <apalano23@gmail.com>
2024-04-23 11:09:29 +0200

    INFO: Updated main.py to check new Bundles

--- src/main.py
+++ src/main.py
@@ -3,18 +3,24 @@
 # Press Maiusc+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from src.drawBundle.graph import AbrImagesHandler as aih
-from src.drawBundle.chart import chartHandler as ch
+from src.drawBundle.chart import ChartHandler as ch
+from src.performanceBundle.timer import Timer as pt
 import numpy as np
+
+
 # Press the green button in the gutter to run the script.
 def abrImage():
     abrImageHandler = aih.AbrImagesHandler()
     g = aih.createExampleGraph()
     aih.drawGraph(g)
     abrImageHandler.saveGraphImg(g, "exampleBtree.png")
+
+
 def printChart():
     ch.printChart()
-if __name__ == '__main__':
-    #abrImage();
+
+
+def saveChart():
     datasetX = np.random.randint(0, 100, size=100)
     datasetY = np.random.randint(0, 100, size=100)
     datasetX.sort();
@@ -22,5 +28,9 @@ if __name__ == '__main__':
     chHandler = ch.ChartHandler()
     chHandler.saveChart(datasetX, datasetY, "X", "Y", "chart.png")
 
+
+if __name__ == '__main__':
+    print(pt.testFunction(saveChart))
+    # abrImage();
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
