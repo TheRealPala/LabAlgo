0515466d6007f4b4788221982f5625ecac219c73
Alessandro Palano <apalano23@gmail.com>
2024-05-11 00:12:40 +0200

    FEAT: Added linkedList BST

---
+++ src/mainBundle/linkedListBundle/LLBSNode.py
@@ -0,0 +1,26 @@
+from src.mainBundle.bst.BSNode import BSNode
+
+
+class LLBSNode(BSNode):
+    def __init__(self, key):
+        super().__init__(key)
+        self.__next = None
+        self.__count = 1
+
+    def getNext(self):
+        return self.__next
+
+    def setNext(self, next):
+        self.__next = next
+
+    def getCount(self):
+        return self.__count
+
+    def setCount(self, count):
+        self.__count = count
+
+    def showAllNextValues(self):
+        current = self
+        while current is not None:
+            print(current)
+            current = current.getNext()
---
+++ src/mainBundle/linkedListBundle/LLBSTree.py
@@ -0,0 +1,59 @@
+from src.mainBundle.bst.BSTree import BSTree
+from src.mainBundle.linkedListBundle.LLBSNode import LLBSNode
+
+
+class LLBSTree(BSTree):
+    def __init__(self):
+        super().__init__()
+
+    def insert(self, key):
+        node = LLBSNode(key)
+        father = None
+        current = self._root
+        insideLinkedList = False
+        while current is not None and not insideLinkedList:
+            father = current
+            if node.getKey() < current.getKey():
+                current = current.getLeft()
+            elif node.getKey() > current.getKey():
+                current = current.getRight()
+            else:
+                insideLinkedList = True
+        node.setFather(father)
+        if father is None:
+            self._root = node
+        else:
+            if not insideLinkedList:
+                if node.getKey() < father.getKey():
+                    father.setLeft(node)
+                else:
+                    father.setRight(node)
+            else:
+                # change bst pointer to child and father (tipo trapianto ma con aggiornamento anche dei figli)
+                node.setFather(father.getFather())
+                node.setCount(father.getCount() + 1)
+                father.setCount(0)
+                if father.getLeft() is not None:
+                    node.setLeft(father.getLeft())
+                    father.getLeft().setFather(node)
+                if father.getRight() is not None:
+                    node.setRight(father.getRight())
+                    father.getRight().setFather(node)
+                if father.__eq__(father.getFather().getLeft()):
+                    father.getFather().setLeft(node)
+                else:
+                    father.getFather().setRight(node)
+
+                father.setFather(node)
+                father.setLeft(None)
+                father.setRight(None)
+                node.setNext(father)
+
+    def find(self, key):
+        current = self._root
+        while current is not None and current.getKey() != key:
+            if key < current.getKey():
+                current = current.getLeft()
+            else:
+                current = current.getRight()
+        return current
