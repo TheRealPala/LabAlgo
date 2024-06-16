fd6a1618102aa8eaee2c92fb13bd4069fa2d615b
pala <apalano23@gmail.com>
2024-06-17 01:43:40 +0200

    INFO:Added print of debug

--- src/main.py
+++ src/main.py
@@ -8,13 +8,18 @@ from drawBundle.images import generateImages
 sys.setrecursionlimit(int(math.pow(10, 8)))
 
 if __name__ == '__main__':
+    print("Generate example images... ")
     generateImages.createImages()
+    print("Run tests... ")
     t = Tester.runAllTests()
+    print("Elaborate results...")
     insertResult = Tester.elaborateInsertResults(t)
+    print("Generate charts...")
     ch = ChartHandler()
     ch.setAction("insert")
     ch.genereateChartFromResults(insertResult)
     findResult = Tester.elaborateFindResults(t)
     ch.setAction("find")
     ch.genereateChartFromResults(findResult)
+    print("Done")
 
